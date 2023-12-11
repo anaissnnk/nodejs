7eae736cd893b7686c37489b69d4d1ce8c50d46d
Anaïs <aekyon.08@gmail.com>
2023-12-11 11:35:58 +0100

    Learn Import Path

--- Day 1/Getting started/server.js
+++ Day 1/Getting started/server.js
@@ -1,2 +1,9 @@
+const path = require("path");
 console.log("Hello World");
 console.log("This is a console log from my server.js file");
+console.log(path.basename(__filename));
+console.log(path.dirname(__filename));
+console.log(path.extname(__filename));
+console.log(path.parse(__filename));
+console.log(path.parse(__filename).base);
+console.log(path.join(__dirname, "test", "hello.html"));
