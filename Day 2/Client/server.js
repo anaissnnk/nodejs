e1c15e0978968313db0b85b3021a7e7b0896e6f0
Anaïs <aekyon.08@gmail.com>
2023-12-12 14:20:08 +0100

    Console.log URL from root

--- Day 2/Client/server.js
+++ Day 2/Client/server.js
@@ -10,8 +10,8 @@ const server = http.createServer()
 //CREATE RESPONSE
 server.on("request", (request, response) => {
   console.log(request.url);
-  if(request.url !== " ") {
-    console.log(path.dirname(__filename));
+  if(request.url === "/") {
+    console.log(path.join(__dirname, "index.html"));
   }
   response.end();
 })
@@ -20,12 +20,3 @@ server.on("request", (request, response) => {
 server.listen(3000, () => {
     console.log("Server started on http://127.0.0.1:3000");
 })
-
-/*
-console.log(path.basename(__filename));
-console.log(path.dirname(__filename));
-console.log(path.extname(__filename));
-console.log(path.parse(__filename));
-console.log(path.parse(__filename).base);
-console.log(path.join(__dirname, "test", "hello.html"));
-*/
