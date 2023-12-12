b56376d71981376f2b624739d98b1b8b23367e11
Anaïs <aekyon.08@gmail.com>
2023-12-12 15:56:34 +0100

    Add html link to all folders

--- Day 2/Client/server.js
+++ Day 2/Client/server.js
@@ -19,28 +19,17 @@ http.createServer(function (req, res) {
       res.write(data);
       return res.end();
     });
+  } else if (req.url ==="/blog") {
+    fs.readFile('blog/index.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
+  } else if (req.url ==="/contact") {
+    fs.readFile('contact/index.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
   }
-  
 }).listen(8080);
-
-//http://localhost:8080
-
-//CREATE RESPONSE
-/*server.on("request", (req, res) => {
-  console.log(req.url);
-  if(req.url === "/") {
-  console.log("This is the root");
-  } else if (req.url === "/about") {
-    console.log("This is the about folder");
-  } else if (req.url === "/blog") {
-    console.log("This is the blog folder");
-  } else if (req.url === "/contact") {
-    console.log("This is the contact folder");
-  }
-  res.end();
-})*/
-
-//LISTEN TO SERVER RESPONSE
-/*server.listen(3000, () => {
-    console.log("Server started on http://127.0.0.1:3000");
-})*/
