69390aaf38b1b2551f6667677e58a71d0df77067
Anaïs <aekyon.08@gmail.com>
2023-12-14 10:21:54 +0100

    Add Day 3 Exericise + Create folder API exercise

---
+++ Day 3/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+//app.listen(3000);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+app.get('/Folder-1', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Folder-1', 'index1.html'));
+})
+
+app.get('/Folder-2', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Folder-2', 'index2.html'));
+})
