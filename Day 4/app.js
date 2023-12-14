35c481b8a0522b41869f9ab7e628a5929a24fb52
Anaïs <aekyon.08@gmail.com>
2023-12-14 17:44:33 +0100

    Create routes to each object in dishes.js

--- Day 4/app.js
+++ Day 4/app.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
 })
 
 //CREATING NEW LIST TO PUSH WITH PREDETERMINED PARAMETERS
-app.post("/dishes/new", (req, res) => {
+app.post("/dishes/test6", (req, res) => {
     const newDish = {
         name: req.body.name,
         timeToPrepare: req.body.timeToPrepare,
@@ -27,16 +27,56 @@ app.post("/dishes/new", (req, res) => {
 
 // //FETCH DATA FROM DISHES.JS
 app.get("/dishes", (req, res) => {
-    res.send(dishes);
+    const dishNames = dishes.map(dish => dish.name);
+    res.send("Heya, here you will find recipes for the following dishes: " + dishNames);
 })
 
 //TARGET SPECIFIC VALUE FROM DISHES.JS
-app.get("/dishes/:id", (req, res) => {
-    const dish = dishes.find(dish => dish.id === parseInt(req.params.id))
-    res.send(dish)
-})
+// app.get("/dishes/:id", (req, res) => {
+//     const dish = dishes.find(dish => dish.id === parseInt(req.params.id))
+//     res.send(dish)
+// })
 
 //DEFINE PORT AND LISTEN
 app.listen(3000, () => {
     console.log("Running on http://localhost:3000")
 });
+
+//TODO: add a "info" page that explains how to use the API
+
+// app.post("/dishes/chocolate-cake", (req, res) => {
+//     const newDish = {
+//         name: req.body.name,
+//         timeToPrepare: req.body.timeToPrepare,
+//         ingredients: req.body.ingredients,
+//         "list of steps": req.body.steps,
+//     }
+//     dishes.push(newDish);
+//     res.status(200).send(dishes)
+// })
+
+app.get("/dishes/chocolate-cake", (req, res) => {
+    const dish = dishes.find(dish => dish.name === "Chocolate Cake")
+    res.send(dish)
+})
+
+app.get("/dishes/matcha-brownies", (req, res) => {
+    const dish = dishes.find(dish => dish.name === "Matcha Brownies")
+    res.send(dish)
+})
+
+app.get("/dishes/hojicha-latte", (req, res) => {
+    const dish = dishes.find(dish => dish.name === "Hojicha Latte")
+    res.send(dish)
+})
+
+app.get("/dishes/buffala-and-pesto-baguette", (req, res) => {
+    const dish = dishes.find(dish => dish.name === "Buffala and Pesto Baguette")
+    res.send(dish)
+})
+
+// app.get("/dishes/test6", (req, res) => {
+//     const dish = dishes.find(dish => dish.name === "test6")
+//     res.send(dish)
+// })
+
--- Day 4/dishes.js
+++ Day 4/dishes.js
@@ -1,13 +1,13 @@
 const dishes = [{
     //FIRST
-    name: "Chocolate Lava Cake",
+    name: "Chocolate Cake",
     timeToPrepare: "40 minutes",
     ingredients: "Dark Chocolate, Butter, Flour, Eggs, Sugar",
-    "list of steps": "Melt chocolate and butter in a pan. In the mean time, whisk eggs with sugar and add flour little by little. Combine both, and cook in the oven for 25-30 minutes.",
+    "list of steps": "Melt chocolate and butter together in a pan. In the mean time, whisk eggs with sugar and add flour little by little. Combine both, and cook in the oven for 25-30 minutes.",
 },
 
 {   //SECOND
-    name: "Matcha Fudgy Brownies",
+    name: "Matcha Brownies",
     timeToPrepare: "30 minutes",
     ingredients: "Matcha, White Chocolate, Butter, Flour, Eggs, Brown Sugar",
     "list of steps": "Melt the butter in a pan until it stars browning. Pour the melted butter in bowl with white chocolate and matcha then stir the ganache, let it cool. Mix the dry ingredients together and add the mixture. Pour the batter into your mold and cook in the oven for 15-20 minutes.",
@@ -21,7 +21,7 @@ const dishes = [{
 },
 
 {   //FOURTH
-    name: "Buffala and Pesto baguette",
+    name: "Buffala and Pesto Baguette",
     timeToPrepare: "30 minutes",
     ingredients: "Baguette, buffala, pesto, sundried tomatoes, pepper, garlic",
     "list of steps": "Bake your baguette in the oven. Cut in half, and cook in a pan and grate garlic. Afterwards, spread pesto, then cut your buffala and spread it on top. Add the sun-sdruned tomatoes and pepper.",
