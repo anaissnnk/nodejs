309de986981c134b8e11ae482fff64940abe796b
Anaïs <aekyon.08@gmail.com>
2023-12-14 11:03:03 +0100

    Listen to server and send root message

--- Day 4/app.js
+++ Day 4/app.js
@@ -1,4 +1,16 @@
+//IMPORT EXPRESS MODULE AND DEFINE APP.JS AS MAIN APPLICATION
 const express = require('express');
 const app = express();
 
-app.listen(3000);
+//TELL COMPUTER HOW TO HANDLE UPCOMING INFORMATION
+app.use(express.json());
+
+//SEND AN OK MESSAGE ON ROOT LEVEL
+app.get("/", (req, res) => {
+    res.send({code: 200, msg:"OK"})
+})
+
+//DEFINE PORT AND LISTEN
+app.listen(3000, () => {
+    console.log("Running on http://localhost:3000")
+});
