2c102af7eb21c63fd15ad82d23057f9cb03b62f5
Anaïs <aekyon.08@gmail.com>
2023-12-14 18:36:07 +0100

    Change dry code to dynamic code for routing

--- Day 4/app.js
+++ Day 4/app.js
@@ -1,3 +1,5 @@
+//TODO: add a "info" page that explains how to use the API
+
 //IMPORT COLLECTION
 import dishes from './dishes.js';
 
@@ -13,23 +15,31 @@ app.get("/", (req, res) => {
     res.send({code: 200, msg:"OK"})
 })
 
-//CREATING NEW LIST TO PUSH WITH PREDETERMINED PARAMETERS
-app.post("/dishes/test6", (req, res) => {
-    const newDish = {
-        name: req.body.name,
-        timeToPrepare: req.body.timeToPrepare,
-        ingredients: req.body.ingredients,
-        "list of steps": req.body.steps,
-    }
-    dishes.push(newDish);
-    res.status(200).send(dishes)
-})
-
 // //FETCH DATA FROM DISHES.JS
 app.get("/dishes", (req, res) => {
     const dishNames = dishes.map(dish => dish.name);
     res.send("Heya, here you will find recipes for the following dishes: " + dishNames);
 })
+
+app.get("/dishes/:name", (req, res) => {
+    const dish = dishes.find(dish => dish.name.toLowerCase() === req.params.name.toLowerCase());
+    if (dish) {
+        res.send(dish);
+    } else {
+        res.status(404).send("Dish not found. Check for typos!");
+    }
+});
+
+//DEFINE PORT AND LISTEN
+app.listen(3000, () => {
+    console.log("Running on http://localhost:3000")
+});
+
+//DRY CODE
+// app.get("/dishes/matcha-brownies", (req, res) => {
+//     const dish = dishes.find(dish => dish.name === "Matcha Brownies")
+//     res.send(dish)
+// })
 
 //TARGET SPECIFIC VALUE FROM DISHES.JS
 // app.get("/dishes/:id", (req, res) => {
@@ -37,13 +47,7 @@ app.get("/dishes", (req, res) => {
 //     res.send(dish)
 // })
 
-//DEFINE PORT AND LISTEN
-app.listen(3000, () => {
-    console.log("Running on http://localhost:3000")
-});
-
-//TODO: add a "info" page that explains how to use the API
-
+//APP.POST
 // app.post("/dishes/chocolate-cake", (req, res) => {
 //     const newDish = {
 //         name: req.body.name,
@@ -55,28 +59,20 @@ app.listen(3000, () => {
 //     res.status(200).send(dishes)
 // })
 
-app.get("/dishes/chocolate-cake", (req, res) => {
-    const dish = dishes.find(dish => dish.name === "Chocolate Cake")
-    res.send(dish)
-})
-
-app.get("/dishes/matcha-brownies", (req, res) => {
-    const dish = dishes.find(dish => dish.name === "Matcha Brownies")
-    res.send(dish)
-})
-
-app.get("/dishes/hojicha-latte", (req, res) => {
-    const dish = dishes.find(dish => dish.name === "Hojicha Latte")
-    res.send(dish)
-})
-
-app.get("/dishes/buffala-and-pesto-baguette", (req, res) => {
-    const dish = dishes.find(dish => dish.name === "Buffala and Pesto Baguette")
-    res.send(dish)
-})
-
+//POST TEST
 // app.get("/dishes/test6", (req, res) => {
 //     const dish = dishes.find(dish => dish.name === "test6")
 //     res.send(dish)
 // })
 
+// //CREATING NEW LIST TO PUSH WITH PREDETERMINED PARAMETERS
+// app.post("/dishes/test6", (req, res) => {
+//     const newDish = {
+//         name: req.body.name,
+//         timeToPrepare: req.body.timeToPrepare,
+//         ingredients: req.body.ingredients,
+//         "list of steps": req.body.steps,
+//     }
+//     dishes.push(newDish);
+//     res.status(200).send(dishes)
+// })
